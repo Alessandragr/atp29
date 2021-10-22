d7c8ab23674f255d7f9d153874da916c777103e5
Alessandragr <alessandra-ribeiro@live.com>
2021-10-22 12:00:17 -0300

    Continuação da atividade, etapa: Main.

--- views/Main.java
+++ views/Main.java
@@ -16,27 +16,39 @@ public class Main {
     }
     public void menu() {
         System.out.println("------Menu de Escolhas------");
-        System.out.println("Digite: \n1 - para Cadastro;\n2 - para Update;\n
-        3 - para Delete;\n4 - para Relatório;");
+        System.out.println("Digite: \n1 - para Cadastro;\n2 - para Update;\n3 - para Delete;\n4 - para Relatório;");
 
         int opcao =scan.nextInt();
         scan.nextLine();
         int escolha =0;
         switch (escolha) {
             case 1:
-                System.out.println("Insira ");
-                break;
+                System.out.println("Insira o ID do produto: ");
+                System.out.println("Insira o modelo do produto: ");
+                System.out.println("Insira o ano do produto: ");
+                System.out.println("Insira o tipo de produto: ");
+                System.out.println("Insira a voltagem do produto: ");
+                System.out.println("Insira a linha do produto: ");
+                System.out.println("Insira o valor do produto: ");
+                return opcao;
             case 2:
-                System.out.println();
-                break;
+            System.out.println("Insira o ID do produto: ");
+            System.out.println("Insira o modelo do produto: ");
+            System.out.println("Insira o ano do produto: ");
+            System.out.println("Insira o tipo de produto: ");
+            System.out.println("Insira a voltagem do produto: ");
+            System.out.println("Insira a linha do produto: ");
+            System.out.println("Insira o valor do produto: ");
+                return opcao;
             case 3:
-                System.out.println();
-                break;
+                System.out.println("Informe o ID do produto: ");
+                return opcao;
             case 4:
-                System.out.println();
-                break;
+                System.out.println("Informe o ID do produto: ");
+                return opcao;
         
             default:
+            System.out.println("Opção inválida!");
                 break;
         }
     }
@@ -50,4 +62,6 @@ public class Main {
         double numero = Double.parseDouble(scan.nextLine());
         return numero;
     }
+
+
 }
